111859336a3d1c99ab768a3c2b6c5db4c09166bb
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2020-04-08 20:00:05 +0530

    added NGWebDriver Demo code

---
+++ src/main/java/pages/CalculatorPage.java
@@ -0,0 +1,84 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.paulhammant.ngwebdriver.ByAngular;
+import com.paulhammant.ngwebdriver.ByAngularButtonText;
+import com.paulhammant.ngwebdriver.ByAngularModel;
+import com.paulhammant.ngwebdriver.NgWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CalculatorPage {
+
+	WebDriver driver;
+	NgWebDriver ngWebDriver;
+	JavascriptExecutor jsDriver;
+
+	ByAngularModel first = ByAngular.model("first");
+	ByAngularModel second = ByAngular.model("second");
+	ByAngularModel operator = ByAngular.model("operator");
+	ByAngularButtonText go = ByAngular.buttonText("Go!");
+
+	By result = By.tagName("h2");
+
+	public CalculatorPage() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		jsDriver = (JavascriptExecutor) driver;
+		ngWebDriver = new NgWebDriver(jsDriver);
+		driver.get("http://localhost:3456/");
+		ngWebDriver.waitForAngularRequestsToFinish();
+
+	}
+
+	public String doSum(String v1, String v2) {
+		driver.findElement(first).clear();
+		driver.findElement(first).sendKeys(v1);
+
+		driver.findElement(second).clear();
+		driver.findElement(second).sendKeys(v2);
+
+		driver.findElement(operator).sendKeys("+");
+		driver.findElement(go).click();
+		ngWebDriver.waitForAngularRequestsToFinish();
+
+		System.out.println(driver.findElement(result).getText());
+		return driver.findElement(result).getText();
+	}
+
+	public String doSub(String v1, String v2) {
+		driver.findElement(first).clear();
+		driver.findElement(first).sendKeys(v1);
+
+		driver.findElement(second).clear();
+		driver.findElement(second).sendKeys(v2);
+
+		driver.findElement(operator).sendKeys("-");
+		driver.findElement(go).click();
+		ngWebDriver.waitForAngularRequestsToFinish();
+
+		System.out.println(driver.findElement(result).getText());
+		return driver.findElement(result).getText();
+	}
+
+	public String doDiv(String v1, String v2) {
+		driver.findElement(first).clear();
+		driver.findElement(first).sendKeys(v1);
+
+		driver.findElement(second).clear();
+		driver.findElement(second).sendKeys(v2);
+
+		driver.findElement(operator).sendKeys("/");
+		driver.findElement(go).click();
+		ngWebDriver.waitForAngularRequestsToFinish();
+
+		System.out.println(driver.findElement(result).getText());
+		return driver.findElement(result).getText();
+	}
+
+}
---
+++ src/test/java/com/tests/CalTest.java
@@ -0,0 +1,39 @@
+package com.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pages.CalculatorPage;
+
+public class CalTest {
+	
+	CalculatorPage calPage;
+	
+	@BeforeTest
+	public void setup(){
+		calPage = new CalculatorPage();
+	}
+	
+	
+	@Test(priority=1)
+	public void sumTest(){
+		String res = calPage.doSum("10", "20");
+		Assert.assertEquals(res, "30");
+	}
+	
+	@Test(priority=2)
+	public void subTest(){
+		String res = calPage.doSub("100", "80");
+		Assert.assertEquals(res, "20");
+	}
+	
+	@Test(priority=3)
+	public void divTest(){
+		String res = calPage.doDiv("20", "2");
+		Assert.assertEquals(res, "10");
+	}
+	
+	
+
+}
